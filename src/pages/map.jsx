f63982fb85c350753ad8ebcf176bfd65f58f3895
Shannon <s38cui@uwaterloo.ca>
2023-01-21 18:45:33 -0500

    drawer

--- src/pages/map.jsx
+++ src/pages/map.jsx
@@ -1,5 +1,22 @@
 import * as React from "react";
+import Drawer from '../components/drawer.jsx';
+import { Link } from "wouter";
+import {
+    Flex,
+    Container,
+    Heading,
+    Stack,
+    Text,
+    Button,
+    Icon,
+    IconProps,
+    useDisclosure,
+  } from '@chakra-ui/react';
 
 export default function Map() {
-
-}
+    return (
+      <Container maxW={'5xl'}>
+            <Drawer/>
+      </Container>
+    );
+  };
