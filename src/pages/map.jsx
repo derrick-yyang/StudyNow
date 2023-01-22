9fc71ca4ef0399b6d9c79d76627a99cc289a1234
Derrick Yang <yangguangzheng122@gmail.com>
2023-01-21 23:20:16 -0500

    Added demo landmarks on map

--- src/pages/map.jsx
+++ src/pages/map.jsx
@@ -1,5 +1,5 @@
 import Drawer from '../components/drawer.jsx';
-import { Map, GoogleApiWrapper } from 'google-maps-react';
+import { Map, GoogleApiWrapper, InfoWindow, Marker } from 'google-maps-react';
 import React, { Component } from 'react';
 import { Link } from "wouter";
 import {
@@ -20,19 +20,55 @@ const mapStyles = {
 };
 
 export class MapContainer extends Component {
+  // state = {
+  //   showingInfoWindow: false,  // Hides or shows the InfoWindow
+  //   activeMarker: {},          // Shows the active marker upon click
+  //   selectedPlace: {}          // Shows the InfoWindow to the selected place upon a marker
+  // };
+  // onMarkerClick = (props, marker, e) =>
+  // this.setState({
+  //   selectedPlace: props,
+  //   activeMarker: marker,
+  //   showingInfoWindow: true
+  // });
+
+  // onClose = props => {
+  //   if (this.state.showingInfoWindow) {
+  //     this.setState({
+  //       showingInfoWindow: false,
+  //       activeMarker: null
+  //     });
+  //   }
+  // };
   render() {
     return (
       <Map
         google={this.props.google}
-        zoom={2}
+        zoom={16}
         style={mapStyles}
         initialCenter={
           {
-            lat: -1.2884,
-            lng: 36.8233
+            // Centre of UW
+            lat: 43.471219, 
+            lng: -80.543044
           }
         }
-      />
+      >
+        {/* Below is M3 */}
+        <Marker 
+          position={{ lat: 43.47330, lng: -80.54411}}
+          onClick={()=>{console.log("click!")}}
+        />
+        {/* Below is STC */}
+        <Marker position={{ lat: 43.470837, lng: -80.54411}} /> 
+        {/* Below is SLC */}
+        <Marker position={{ lat: 43.47185265218329, lng: -80.54534359583154}} />  
+        {/* Below is DC */}
+        <Marker position={{ lat: 43.47291357044051, lng: -80.54269479945117}} />  
+        {/* Below is E7 */}
+        <Marker position={{ lat: 43.47304812731901, lng: -80.53952274900837}} />  
+         
+      </Map>
     );
   }
 }
