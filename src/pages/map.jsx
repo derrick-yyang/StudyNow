dd312a93c496f7fae57be302b7cd992870154901
Jerry Cheng <j983chen@uwaterloo.ca>
2023-01-21 20:33:20 -0500

    remove api key

--- src/pages/map.jsx
+++ src/pages/map.jsx
@@ -38,7 +38,7 @@ export class MapContainer extends Component {
 }
 
 export default GoogleApiWrapper({
-  apiKey: 'AIzaSyAuVVDjHQcMKXWF-23o9vPqCSyQgkJb7R4'
+  apiKey: '[API KEY]'
 })(MapContainer);
 
 // export default function MapPage() {
